0da113fb81409cc46bcedbbf769734e147cba18f
wyldetech <69891468+wyldetech@users.noreply.github.com>
2025-05-13 17:22:40 +1000

    Update scraper.rb

--- scraper.rb
+++ scraper.rb
@@ -35,7 +35,7 @@
 #end
 
 
-
+# encoding: utf-8
 require 'scraperwiki'
 require 'mechanize'
 
@@ -44,29 +44,29 @@ a = Mechanize.new
 url = "https://portal.planbuild.tas.gov.au/external/advertisement/search"
 
 a.get(url) do |page|
-  page.search('.advertisement-result-row').each do |row|
-    address = row.at('.col-xs-8').text.strip
-    council_reference = row.at('.col-xs-4').text.strip
-    id = row['id']
+  page.search('.advertisement-result-row').each do |row|
+    address = row.at('.col-xs-8').text.strip
+    council_reference = row.at('.col-xs-4').text.strip
+    id = row['id']
 
-    # Fetch the detailed page for each advertisement
-    detail_url = "https://portal.planbuild.tas.gov.au/external/advertisement/#{id}"
-    detail_page = a.get(detail_url)
+    # Fetch the detailed page for each advertisement
+    detail_url = "https://portal.planbuild.tas.gov.au/external/advertisement/#{id}"
+    detail_page = a.get(detail_url)
 
-    description = detail_page.at('.toastui-editor-contents p[data-nodeid="32"]').text.strip
-    on_notice_to = detail_page.at('#advertisementEndDate').text.strip
+    description = detail_page.at('.toastui-editor-contents p[data-nodeid="32"]').text.strip
+    on_notice_to = detail_page.at('#advertisementEndDate').text.strip
 
-    record = {
-      'council_reference' => council_reference,
-      'address' => address,
-      'description' => description,
-      'on_notice_to' => Date.parse(on_notice_to).to_s,
-      'date_scraped' => Date.today.to_s,
-      'info_url' => detail_url,
-      'comment_authority' => "City of Hobart",
-      'comment_email' => "representation@hobartcity.com.au"
-    }
+    record = {
+      'council_reference' => council_reference,
+      'address' => address,
+      'description' => description,
+      'on_notice_to' => Date.parse(on_notice_to).to_s,
+      'date_scraped' => Date.today.to_s,
+      'info_url' => detail_url,
+      'comment_authority' => "City of Hobart",
+      'comment_email' => "representation@hobartcity.com.au"
+    }
 
-    ScraperWiki.save_sqlite(['council_reference'], record)
-  end
+    ScraperWiki.save_sqlite(['council_reference'], record)
+  end
 end
