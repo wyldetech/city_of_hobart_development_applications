fa61642b1fc9453f2470e1c7ddf9691e95740252
wyldetech <69891468+wyldetech@users.noreply.github.com>
2025-05-14 11:33:39 +1000

    Update scraper.rb

--- scraper.rb
+++ scraper.rb
@@ -13,14 +13,15 @@ site.search('.advertisement-result-row').each do |row|
   appl = a.get("https://portal.planbuild.tas.gov.au/external/advertisement/#{row.id}")
   puts appl.inspect    
   
-  record = {
-        'council_reference' => council_reference,
-        'address' => address,
-        'description' => description,
-        'on_notice_to' => Date.parse(on_notice_to).to_s,
-        'date_scraped' => Date.today.to_s,
-        'info_url' => detail_url,
-        'comment_authority' => "City of Hobart",
-        'comment_email' => "representation@hobartcity.com.au"
-      }
+end
+  #record = {
+  #      'council_reference' => council_reference,
+  #      'address' => address,
+  #      'description' => description,
+  #      'on_notice_to' => Date.parse(on_notice_to).to_s,
+  #      'date_scraped' => Date.today.to_s,
+  #      'info_url' => detail_url,
+  #      'comment_authority' => "City of Hobart",
+  #      'comment_email' => "representation@hobartcity.com.au"
+  #    }
 
